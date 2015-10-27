26cdcfe92e0bc620d316e54806e2bcfcba91cb52
Fabian Schuh <mail@xeroc.org>
2015-10-27 14:49:57 +0100

    Updates to the bitshares-2-cli-wallet

--- build/bitshares-2-cli-wallet/docker/start.sh
+++ build/bitshares-2-cli-wallet/docker/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ -e "/conf/cli-wallet/wallet.json" ]; then
+ echo "Copying predefined wallet.json file"
+ cp -v /conf/cli-wallet/wallet.json /wallet.json
+fi
+
 if [ -n "$NODE_PORT_8090_TCP_ADDR" ]; then
+ env
  /bitshares-2/programs/cli_wallet/cli_wallet \
                         -s ws://${NODE_PORT_8090_TCP_ADDR}:${NODE_PORT_8090_TCP_PORT} \
                         -H 0.0.0.0:8092 \
